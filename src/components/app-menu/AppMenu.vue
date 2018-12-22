585ab85c4b4dc8aae1cb27aa909ef48fc50125a0
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2018-12-22 23:40:30 +0600

    AppMenu

--- src/components/app-menu/AppMenu.vue
+++ src/components/app-menu/AppMenu.vue
@@ -10,25 +10,85 @@
       router-link.item(to="/")
         .title ГЛАВНАЯ
         .desc {{ $route.path === '/' ? 'Вы на главной' : 'Перейти на главную' }}
-      .item.right(to="/ttt")
+      .item.right(
+        :class="{open: flags.taharat}"
+        @click="flags.taharat = !flags.taharat, closeAll('taharat')"
+      )
         .title ТАХАРАТ
         .desc Очищение
-      .item.right(to="/ttt")
-        .title НАМАЗ
-        .desc Все виды намаза
-      .item.right(to="/ttt")
+      .item.right(
+        :class="{open: flags.tutoring}"
+        @click="flags.tutoring = !flags.tutoring, closeAll('tutoring')"
+      )
         .title ОБУЧЕНИЕ
         .desc Видео-фото уроки
+      .item.right(
+        :class="{open: flags.namaz}"
+        @click="flags.namaz = !flags.namaz, closeAll('namaz')"
+      )
+        .title НАМАЗ
+        .desc Все виды намаза
 
-    menu.taharat
+    menu.submenu(v-show="flags.taharat")
       img.image(src="./taharat-bg.jpg")
       nav.items
-        router-link.item(to="/xxx")
+        router-link.item(to="#")
           .title ГУСЛЬ
           .desc Полное омовение
-        router-link.item(to="/xxx")
+        router-link.item(to="#")
           .title ВУДУ
           .desc Малое омовение
+
+    menu.submenu(v-show="flags.tutoring")
+      img.image(src="./tutoring-bg.jpg")
+      nav.items
+        router-link.item(to="#")
+          .title МОЙ ПЕРВЫЙ НАМАЗ
+          .desc Обзор для новичков
+        router-link.item(to="#")
+          .title НАМАЗ: ПОДРОБНО
+          .desc Детализированый обзор
+        router-link.item(to="#")
+          .title ВИДЕО УРОКИ
+          .desc Омовение, намаз
+
+    menu.submenu(v-show="flags.namaz")
+      img.image(src="./namaz-bg.jpg")
+      nav.items
+        .item.right
+          .title ОБЩАЯ ИНФОРМАЦИЯ
+          .desc К прочтению
+        .item.right(
+          :class="{open: flags.fard}"
+          @click="flags.fard = !flags.fard, closeAll('namaz', 'fard')"
+        )
+          .title ФАРД НАМАЗЫ
+          .desc Обязательные
+        .item.right
+          .title ВАДЖИБ НАМАЗЫ
+          .desc Близко к обязательным
+        .item.right
+          .title СУННАТ НАМАЗЫ
+          .desc Желательные
+
+      menu.submenu(v-show="flags.fard")
+        .filler
+        nav.items
+          router-link.item(to="#")
+            .title ФАДЖР
+            .desc Утренний
+          router-link.item(to="#")
+            .title ЗУХР
+            .desc Полуденный
+          router-link.item(to="#")
+            .title АСР
+            .desc Послеполуденный
+          router-link.item(to="#")
+            .title МАГРИБ
+            .desc Вечерний
+          router-link.item(to="#")
+            .title ИША
+            .desc Ночной
 
     .socials-block
       Socials
@@ -38,6 +98,25 @@
 import Socials from '@/components/socials/Socials.vue';
 
 export default {
+  data() {
+    return {
+      flags: {
+        taharat: false,
+        tutoring: false,
+        namaz: false,
+        fard: false,
+      },
+    };
+  },
+  methods: {
+    closeAll(exceptL1, exceptL2) {
+      Object.keys(this.flags)
+        .filter(k => k !== exceptL1 && k !== exceptL2)
+        .forEach((k) => {
+          this.flags[k] = false;
+        });
+    },
+  },
   components: {
     Socials,
   },
@@ -105,25 +184,37 @@ export default {
       color: #fff;
     }
   }
-  &.right {
-    background-image: url(./right.png);
-    background-repeat: no-repeat;
-    background-position: 197px - 22px;
-  }
   &:hover {
     &.right {
       background-image: url(./right-active.png);
     }
   }
+  &.right {
+    background-image: url(./right.png);
+    background-repeat: no-repeat;
+    background-position: 197px - 22px;
+    &.open {
+      background-image: url(./right-open.png);
+      background-color: #07162d;
+      .title {
+        color: #fff;
+      }
+    }
+  }
 }
-.taharat {
+.submenu {
   z-index: 10000;
   position: absolute;
   top: 0;
   right: -197px;
+  width: 197px;
   border-left: 1px solid #313e54;
   .image {
     display: block;
+  }
+  .filler {
+    height: 3px;
+    background-color: #081934;
   }
 }
 .socials-block {

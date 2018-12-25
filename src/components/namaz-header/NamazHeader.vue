04bcf8c9a6f1454df8c2d0d2be30a41ec0e93806
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2018-12-25 18:46:46 +0600

    NamazHeader

--- src/components/namaz-header/NamazHeader.vue
+++ src/components/namaz-header/NamazHeader.vue
@@ -3,7 +3,14 @@
     :class="{'desc-open': descOpen}"
     :style="{'background-image': `url(${bgImage})`}"
   )
-    section.info
+    section.info(:class="type")
+      h1.title {{ title }}
+      .short-desc {{ shortDesc }}
+      .details
+        .type {{ typeText }}
+        .clocks {{ time }}
+          span.foot-note  *
+
     section.desc(:class="{closed: !descOpen}")
       .desc-text {{ desc }}
     .button
@@ -20,6 +27,21 @@ import AppButton from '@/components/AppButton.vue';
 
 export default {
   props: {
+    type: String,
+    title: String,
+    shortDesc: String,
+    rakaats1: String,
+    rakaats1Title: String,
+    rakaats2: String,
+    rakaats2Title: String,
+    rakaats3: String,
+    rakaats4Title: String,
+    button1: String,
+    button1Link: String,
+    button2: String,
+    button2Link: String,
+    button3: String,
+    button3Link: String,
     desc: String,
     bgImage: String,
   },
@@ -27,6 +49,26 @@ export default {
     return {
       descOpen: false,
     };
+  },
+  computed: {
+    typeText() {
+      switch (this.type) {
+        case 'fadjr':
+          return 'Утро';
+
+        default:
+          return '???';
+      }
+    },
+    time() {
+      switch (this.type) {
+        case 'fadjr':
+          return '06:59 - 07:42';
+
+        default:
+          return '???';
+      }
+    },
   },
   components: {
     AppButton,
@@ -43,7 +85,46 @@ export default {
   }
 }
 .info {
+  width: 480px + 2 * 75px;
   height: 445px;
+  padding: 65px 75px;
+  .title {
+    font-family: $pt-sans;
+    font-weight: bold;
+    font-size: 25px;
+    padding-bottom: 16px;
+    margin-bottom: 20px;
+    border-bottom: 1px solid #b8c2cb;
+  }
+  .short-desc {
+    font-weight: 300;
+    line-height: 26px;
+  }
+  .details {
+    display: flex;
+    align-items: center;
+    height: 80px;
+    border-top: 1px solid #b8c2cb;
+    border-bottom: 1px solid #b8c2cb;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    > * {
+      display: flex;
+      justify-content: center;
+      align-items: flex-end;
+      flex-grow: 1;
+      height: 50px;
+      line-height: 1;
+    }
+  }
+  .clocks {
+    background: url(./clocks.png) no-repeat top;
+  }
+  &.fadjr {
+    .type {
+      background: url(./fadjr.png) no-repeat top;
+    }
+  }
 }
 .desc {
   position: relative;

18a27e305248ddc7f190750afb350f94dec1ae73
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2019-01-07 15:26:46 +0600

    Icons

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,12 @@ module.exports = {
   },
   pwa: {
     themeColor: '#1a3157',
+    iconPaths: {
+      favicon16: 'icons/icon-48x48.png',
+      favicon32: 'icons/icon-96x96.png',
+      appleTouchIcon: 'icons/icon-152x152.png',
+      msTileImage: 'icons/icon-144x144.png',
+      maskIcon: '',
+    },
   },
 };

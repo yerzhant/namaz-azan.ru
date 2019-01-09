123759608825ff7dc9162178a89a50c3b94f3db3
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2019-01-09 21:39:50 +0600

    Fixed build

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   configureWebpack: {
     performance: {
-      maxEntrypointSize: 256000,
-      maxAssetSize: 512000,
+      maxEntrypointSize: 350 * 1024,
+      maxAssetSize: 700 * 1024,
       assetFilter: file => !(/\.(map|mp3)$/.test(file)),
     },
   },

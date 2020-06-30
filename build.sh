6bc35bb106dc668eae67d2f171e1535dce41038d
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2020-06-30 07:34:06 +0600

    Upload image to aws

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 export NAMAZ_VERSION=0.1.0
-docker build -t namaz:${NAMAZ_VERSION} .
-# docker push reg.cicd.onlinebus.kz/nurbus-front:${NURBUS_FRONT_VERSION}
+docker build -t 485615673038.dkr.ecr.eu-central-1.amazonaws.com/namaz.azan.ru:${NAMAZ_VERSION} .
+docker push 485615673038.dkr.ecr.eu-central-1.amazonaws.com/namaz.azan.ru:${NAMAZ_VERSION}

67e8fb33316b9d25a2403c9f5c939c640c4b793d
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2020-07-05 05:20:30 +0600

    Disable access logging

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-export NAMAZ_VERSION=0.1.0
+export NAMAZ_VERSION=1.0.0
 docker build -t 485615673038.dkr.ecr.eu-central-1.amazonaws.com/namaz.azan.ru:${NAMAZ_VERSION} .
 docker push 485615673038.dkr.ecr.eu-central-1.amazonaws.com/namaz.azan.ru:${NAMAZ_VERSION}

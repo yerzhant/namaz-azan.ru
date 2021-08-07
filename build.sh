2b784b70de6e6c066f15c7e862429d9e716b0219
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-08-07 11:55:00 +0600

    1.2

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-export NAMAZ_VERSION=1.1.1
-# docker build -t 485615673038.dkr.ecr.eu-central-1.amazonaws.com/namaz.azan.ru:${NAMAZ_VERSION} .
-# docker push 485615673038.dkr.ecr.eu-central-1.amazonaws.com/namaz.azan.ru:${NAMAZ_VERSION}
-docker build -t registry.digitalocean.com/azan-ru/namaz.azan.ru:${NAMAZ_VERSION} .
-docker push registry.digitalocean.com/azan-ru/namaz.azan.ru:${NAMAZ_VERSION}
+export NAMAZ_VERSION=1.2.0
+docker build -t reg.azan.ru/namaz:${NAMAZ_VERSION} .
+docker push reg.azan.ru/namaz:${NAMAZ_VERSION}

00fb3dc09894752340e1c7a3e3e5aa77dc1f699a
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2023-01-07 01:20:03 +0600

    Fix route prefix

--- src/mixins/routePrefix.js
+++ src/mixins/routePrefix.js
@@ -3,7 +3,7 @@ import routeGender from "@/utils/routeGender";
 export default {
   computed: {
     routePrefix() {
-      const { madhhab, gender } = this.$store.state;
+      const { madhhab, gender } = this.store;
       return `/${madhhab}/${routeGender.to(gender)}`;
     },
   },

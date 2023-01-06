4a5437e011f5fff6fcc323b1bf056923d9501fb5
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2023-01-07 00:04:23 +0600

    Fix router

--- src/router.js
+++ src/router.js
@@ -1,15 +1,11 @@
-import Vue from "vue";
-import Router from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import RouteGender from "@/utils/routeGender";
 import AppSelect from "@/views/app-select/AppSelect.vue";
 import AppMain from "@/views/app-main/AppMain.vue";
 import store from "./store";
 
-Vue.use(Router);
-
-const router = new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
+const router = new createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
@@ -24,44 +20,32 @@ const router = new Router({
     },
     {
       path: "/:madhhab/:gender/tour-salah",
-      component: () =>
-        import(
-          /* webpackChunkName: "solaatil-uulaa" */ "./views/tour/Tour.vue"
-        ),
+      component: () => import("./views/tour/Tour.vue"),
       name: "tour-salah",
     },
     {
       path: "/:madhhab/:gender/:section(shart|al-ahkaamul-aammah)/:type",
-      component: () =>
-        import(/* webpackChunkName: "shart" */ "./views/shart/Shart.vue"),
+      component: () => import("./views/shart/Shart.vue"),
       name: "shart",
     },
     {
       path: "/:madhhab/:gender/tahharah/:type(wudu)",
-      component: () =>
-        import(/* webpackChunkName: "wudu" */ "./views/wudu/Wudu.vue"),
+      component: () => import("./views/wudu/Wudu.vue"),
       name: "wudu",
     },
     {
       path: "/:madhhab/:gender/tahharah/:type",
-      component: () =>
-        import(
-          /* webpackChunkName: "tahharah" */ "./views/tahharah/Tahharah.vue"
-        ),
+      component: () => import("./views/tahharah/Tahharah.vue"),
       name: "tahharah",
     },
     {
       path: "/:madhhab/:gender/:type(mareed)/:subType",
-      component: () =>
-        import(
-          /* webpackChunkName: "namaz" */ "./views/namaz-compare/NamazCompare.vue"
-        ),
+      component: () => import("./views/namaz-compare/NamazCompare.vue"),
       name: "namaz-compare",
     },
     {
       path: "/:madhhab/:gender/:type/:subType",
-      component: () =>
-        import(/* webpackChunkName: "namaz" */ "./views/namaz/Namaz.vue"),
+      component: () => import("./views/namaz/Namaz.vue"),
       name: "namaz",
     },
     {

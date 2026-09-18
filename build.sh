9ee9d7c9cae3e7001fdc038463a190b9e6798c1f
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2026-09-18 11:15:38 +0300

    ru -> info

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 export NAMAZ_VERSION=2.7.6
-docker build -t reg.azan.ru/namaz:${NAMAZ_VERSION} .
-# docker push reg.azan.ru/namaz:${NAMAZ_VERSION}
+docker build -t reg.azan.info/namaz:${NAMAZ_VERSION} .
+# docker push reg.azan.info/namaz:${NAMAZ_VERSION}
